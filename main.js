d2665ce318347dc1701ee2c95d8f0920b97dbaaf
marioecg <hey@marioecg.com>
2022-10-23 14:27:13 -0600

    move save funciton to script block

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let canvas, sh, time
+let canvas, sh, t
 
 function preload() {
     sh = loadShader('./shaders/vert.glsl', './shaders/frag.glsl')
@@ -17,11 +17,11 @@ function draw() {
     // sets the shader
     shader(sh)
 
-    time = millis() / 1000
+    t = millis() / 1000
 
     // updates shader uniforms
     sh.setUniform('uResolution', [width, height])
-    sh.setUniform('uTime', time)
+    sh.setUniform('uTime', t)
 
     // creates a shape with the shader applied
     rect(0, 0, width, height)
@@ -32,29 +32,3 @@ function windowResized() {
 
     sh.setUniform('uResolution', [width, height])
 }
-
-// Save frame on CMD/CTRL + SHIF + S
-window.addEventListener('keydown', (e) => {
-    if ((window.navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey) && e.keyCode == 83) {
-        e.preventDefault()
-
-        let date = new Date()
-        let year = date.getFullYear()
-        let month = date.getMonth() + 1
-        let day = date.getUTCDate()
-        let hour = date.getHours()
-        let mins = date.getMinutes()
-        let secs = date.getSeconds()
-    
-        let timestamp = `${year}-${month}-${day}-${hour}-${mins}-${secs}`        
-
-        save(canvas, timestamp)
-    }
-}, false)
-
-// Save video on CMD/CTRL + SHIF + S
-window.addEventListener('keydown', (e) => {
-    if ((window.navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey) && e.keyCode == 86) {
-        e.preventDefault()
-    }
-}, false)
