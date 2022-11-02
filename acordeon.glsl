07013c0cea803633c6b49e6e6de5580ebd735ea0
ilithya <ilithya.net@gmail.com>
2022-11-02 09:51:07 +0100

    Corregir nombre de seccion

--- acordeon.glsl
+++ acordeon.glsl
@@ -16,7 +16,7 @@ void main() {
 
   // Luz es utilizada para mostrar colores (rgb)
 
-  // PORCENTAJES DE RGB
+  // RGB EN PORCENTAJES
 
   // Valores de cada componente de color (rgb) son expresados en porcentajes (0% - 100%)
   // Cada porcentaje es dividido entre 100% para obtener 
