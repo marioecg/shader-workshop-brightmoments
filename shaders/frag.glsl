3e037b380a453c322c5770f03d4dc2dd76d55ebc
marioecg <hey@marioecg.com>
2022-11-02 13:22:55 -0700

    comments

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -1,7 +1,7 @@
 precision mediump float;
 
 void main() {    
-  vec3 color = vec3(1.0, 0.0, 0.0);
+  vec3 color = vec3(1.0, 1.0, 1.0); // RGB
   
-  gl_FragColor = vec4(color, 1.0);
+  gl_FragColor = vec4(color, 1.0); // Color de salida RGBA
 }
