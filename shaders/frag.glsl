80e3c6f3ddca3b83d32dfe765f9bb5f146e6e6a6
marioecg <hey@marioecg.com>
2022-11-02 19:07:05 -0700

    comments

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -1,7 +1,17 @@
-precision mediump float;
+// Define la precisión de bits de los números de punto flotante
+precision mediump float; 
+
+varying vec2 vUv;
 
 void main() {    
-  vec3 color = vec3(1.0, 1.0, 1.0); // RGB
-  
-  gl_FragColor = vec4(color, 1.0); // Color de salida RGBA
+  // RGB, el color será blanco porque todos los canales son igual a 1.0
+  vec3 color = vec3(1.0, 1.0, 1.0); 
+
+  // 1) Cómo puedes obtener otro color, por ejemplo el rojo, magenta o amarillo?
+
+  // 2) Usa las coordenadas provenientes del vertex para mostrar un gradiente de colores,
+  //    color = vec3(vUv, 0.0);
+
+  // Colo final de salida RGBA
+  gl_FragColor = vec4(color, 1.0); 
 }
--- shaders/vert.glsl
+++ shaders/vert.glsl
@@ -1,11 +1,15 @@
+// Define la precisión de bits de los números de punto flotante
 precision mediump float;
 
+// Coordenadas
 attribute vec2 aTexCoord;
+// Vértices de los triángulos que forman el cuadrado (o "quad")
 attribute vec3 aPosition;
 
 varying vec2 vUv;
 
 void main() {
+  // Pasa las coordinadas al fragment shader en una variable vec2
   vUv = aTexCoord;
 
   vec4 pos = vec4(aPosition, 1.0);
