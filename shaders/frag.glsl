8841a84a302fcd49f994582fb9a1730cd561ea36
marioecg <hey@marioecg.com>
2022-11-08 17:56:50 -0600

    update branch

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -9,6 +9,10 @@ float map(float value, float min1, float max1, float min2, float max2) {
   return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
 }
 
+float random(vec2 st) {
+  return fract(sin(dot(st, vec2(12.9898, 78.233))) * 43758.5453);
+}
+
 void main() {    
   vec2 uv = vUv;
   float t = uTime;
@@ -24,6 +28,10 @@ void main() {
   float sizePulse = map(cos(t * 0.5), -1.0, 1.0, 0.3, 0.6);
   circle = smoothstep(sizePulse, 0.0, circle);
   color += circle;  
+
+  // Dar un poco de textura al color
+  float grain = random(vUv);
+  color += grain * 0.05;
 
   // Color final de salida RGBA
   gl_FragColor = vec4(color, 1.0); 
