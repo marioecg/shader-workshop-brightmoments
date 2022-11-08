b516e12c8224375b6d144a3c9979c4d6a6025125
marioecg <hey@marioecg.com>
2022-11-08 17:55:49 -0600

    sunrise branch

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -1,8 +1,6 @@
 // Define la precisión de bits de los números de punto flotante
 precision mediump float; 
 
-#define PI 3.1415926538
-
 uniform float uTime;
 
 varying vec2 vUv;
@@ -11,46 +9,21 @@ float map(float value, float min1, float max1, float min2, float max2) {
   return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
 }
 
-float random(vec2 st) {
-  return fract(sin(dot(st, vec2(12.9898, 78.233))) * 43758.5453);
-}
-
-vec2 rotate(vec2 pos, float a) {
-	float s = sin(a);
-	float c = cos(a);
-	mat2 m = mat2(c, -s, s, c);
-	return m * pos;
-}
-
-float rect(vec2 st, vec2 size){
-	size = 0.25-size*0.25;
-  vec2 uv = step(size,st*(1.0-st));
-	return uv.x*uv.y;
-}
-
 void main() {    
-  // Guardamos las variantes o uniformes en una variable para poder modificarlas después
   vec2 uv = vUv;
   float t = uTime;
 
-  // RGB, el color será blanco porque todos los canales son igual a 1.0
-  vec3 color = vec3(1.0, 1.0, 1.0); 
-  // También existen otras formas de definir la variable vector
-  // vec3 color = vec3(1.0);
-  // o 
-  // vec3 color;
-  // color.r = 1.0;
-  // color.g = 1.0;
-  // color.b = 1.0;
+  // Gradiente vertical
+  vec3 bottom = vec3(0.922, 0.369, 0.29);
+  vec3 top = vec3(0.251, 0.22, 0.369);
+  vec3 color = mix(bottom, top, smoothstep(0.0, 1.0, vUv.y));
 
-  color = vec3(0.0);
-  vec2 fuv = fract(vUv * 5.0);
-  color.rg = fuv;
-
-  // Cómo puedes obtener otro color, por ejemplo el rojo, magenta o amarillo? uv.x / uv.y
-  // uv.x = uv.x;
-  // uv.y = 1.0 - uv.y;
-  // color.rgb = vec3(sin(vUv.x * 30.0));
+  // Circulo pulsando
+  vec2 cuv = vUv - vec2(0.5, 0.0);
+  float circle = length(cuv);
+  float sizePulse = map(cos(t * 0.5), -1.0, 1.0, 0.3, 0.6);
+  circle = smoothstep(sizePulse, 0.0, circle);
+  color += circle;  
 
   // Color final de salida RGBA
   gl_FragColor = vec4(color, 1.0); 
