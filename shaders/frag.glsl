017984fb8919a559294f081ab884c701f3eb3bf2
marioecg <hey@marioecg.com>
2022-11-08 18:01:14 -0600

    update

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -5,33 +5,35 @@ uniform float uTime;
 
 varying vec2 vUv;
 
-float map(float value, float min1, float max1, float min2, float max2) {
-  return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
-}
-
 float random(vec2 st) {
   return fract(sin(dot(st, vec2(12.9898, 78.233))) * 43758.5453);
+}
+
+float rect(vec2 st, vec2 size) {
+  size = 0.25 - size * 0.25;
+  vec2 uv = step(size, st * (1.0 - st));
+  return uv.x * uv.y;
 }
 
 void main() {    
   vec2 uv = vUv;
   float t = uTime;
 
-  // Gradiente vertical
-  vec3 bottom = vec3(0.922, 0.369, 0.29);
-  vec3 top = vec3(0.251, 0.22, 0.369);
-  vec3 color = mix(bottom, top, smoothstep(0.0, 1.0, vUv.y));
+  // Color del cuadrado y del fondo
+  vec3 inner = vec3(0.937, 0.937, 0.937);
+  vec3 outer = vec3(0.835, 0.839, 0.839);
 
-  // Circulo pulsando
-  vec2 cuv = vUv - vec2(0.5, 0.0);
-  float circle = length(cuv);
-  float sizePulse = map(cos(t * 0.5), -1.0, 1.0, 0.3, 0.6);
-  circle = smoothstep(sizePulse, 0.0, circle);
-  color += circle;  
+  // Movemos un poco las coordenadas en Y para el cuadrado interior
+  vec2 suv = uv + vec2(0.0, 0.05);
+  vec2 size = vec2(0.45);
+  float square = rect(suv, size);
+
+  // Mezclamos los colores en base al area del cuadrado
+  vec3 color = mix(outer, inner, square);
 
   // Dar un poco de textura al color
   float grain = random(vUv);
-  color += grain * 0.05;
+  color += grain * 0.1;  
 
   // Color final de salida RGBA
   gl_FragColor = vec4(color, 1.0); 
